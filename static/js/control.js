9b41e4679cacc3353e6b17ec2144dc0db7af9d6c
Delong Fu <delong.fu@hulu.com>
2012-08-04 12:28:00 -0700

    back inplemention complete

--- static/js/control.js
+++ static/js/control.js
@@ -6,6 +6,7 @@ $(function() {
     var $next = $(".next");
     var $prev = $(".prev");
     var $slides_summaries = $("#slides_summaries");
+    var $slides_container = $("#slides_container");
 
     var cur_slide = 0;
     var num_slides = 0; 
@@ -13,9 +14,13 @@ $(function() {
     $.getJSON("data/slides.json", function(json) {
         $.each(json, function(){
             $slides_summaries.append("<div class='summary'>" + this.summary + "</div>");
+            $slides_container.append("<div class='slide'>" + this.slide + "</div>");
             num_slides += 1;
         });
-        $("#slides_summaries").slides();
+        if ($slides_summaries.length > 0)
+            $slides_summaries.slides();
+        if ($slides_container.length > 0)
+            $slides_container.slides();
     });
 
     // Binding the controls
@@ -23,20 +28,20 @@ $(function() {
         if (cur_slide > 0){
             socket.emit('flip', -1);
         }
-        // return false;
     });
     $next.bind('click', function() {
         if (cur_slide < num_slides - 1){
             socket.emit('flip', 1);
         }
-        // return false;
     });
 
     // Bind events to the socket
     socket.on('flip', function(slide) {
         console.log(slide);
-
-        $("#slides_summaries").goto(cur_slide, slide);
+        if ($slides_summaries.length > 0)
+            $("#slides_summaries").goto(cur_slide, slide);
+        if ($slides_container.length > 0)
+            $("#slides_container").goto(cur_slide, slide);
         cur_slide = slide;
     });
 
